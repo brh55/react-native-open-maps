a16b8b1c10d0ccc876ebee59f9fbabb619f0d7e7
Bryce <18741882+brycelund@users.noreply.github.com>
2018-04-05 14:32:39 -0600

    Pass location name along with coordinates

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Linking, Platform } from 'react-native';
 
-export default function open({latitude, longitude, zoomLevel, provider}) {
+export default function open({latitude, longitude, zoomLevel, name, provider}) {
 	// Execute link
-	createOpenLink({latitude, longitude, zoomLevel, provider})();
+	createOpenLink({latitude, longitude, zoomLevel, name, provider})();
 }
 
-export function createOpenLink({latitude, longitude, zoomLevel = 15, provider}) {
+export function createOpenLink({latitude, longitude, zoomLevel = 15, name, provider}) {
 	if (!provider) {
 		defaultProvider = (Platform.OS === 'ios') ? 'apple' : 'google';
 	}
 
 	const mapProvider = provider || defaultProvider;
 	// Allow override provider, otherwise use the default provider
-	const mapLink = createMapLink({latitude, longitude, zoomLevel, provider:  mapProvider});
+	const mapLink = createMapLink({latitude, longitude, zoomLevel, name, provider:  mapProvider});
 
 	// Returns a delayed function that opens when executed
 	return () => Linking.openURL(mapLink).catch(err => console.error('An error occurred', err));
 }
 
-export function createMapLink({latitude, longitude, zoomLevel = 15, provider = 'google'}) {
+export function createMapLink({latitude, longitude, zoomLevel = 15, name = null, provider = 'google'}) {
 	const link = {
-		'google': `http://maps.google.com/maps?q=${latitude},${longitude}&z=${zoomLevel}`,
-		'apple': `http://maps.apple.com/?q=${latitude},${longitude}&z=${zoomLevel}`
+		'google': `http://maps.google.com/maps?ll=${latitude},${longitude}&z=${zoomLevel}&q=${name}`,
+		'apple': `http://maps.apple.com/?ll=${latitude},${longitude}&z=${zoomLevel}&q=${name}`
 	};
 
 	return link[provider];
