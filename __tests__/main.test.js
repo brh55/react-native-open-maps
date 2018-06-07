250cab7d9dd435813f870ef308c51eaff9803f0c
brh55 <brandonhim@live.com>
2018-06-06 17:22:32 -0700

    Fix pin issue, and add deep link support

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,22 +1,27 @@
 import { createMapLink, createOpenLink } from '../.';
 
+const query = 'Yosemite National Park';
+const expectedQueryName = encodeURI(query);
+
 const options = {
 	latitude: 10.02134,
 	longitude: -29.21322,
 	zoom: 15,
-	name: 'pin name'
+	query
 }
 
 test('Create proper links', () => {
 	// Google default link
-	expect(createMapLink(options)).toEqual('http://maps.google.com/maps?ll=10.02134,-29.21322&z=15&q=pin%20name');
+	expect(createMapLink(options)).toEqual(`http://maps.google.com/maps?ll=10.02134,-29.21322&z=15&q=${expectedQueryName}`);
 	// Google map link
-	expect(createMapLink({ ...options, provider: 'google' })).toEqual('http://maps.google.com/maps?ll=10.02134,-29.21322&z=15&q=pin%20name');
+	expect(createMapLink({ ...options, provider: 'google' })).toEqual(`http://maps.google.com/maps?ll=10.02134,-29.21322&z=15&q=${expectedQueryName}`);
 	// Apple map link
-	expect(createMapLink({ ...options, provider: 'apple' })).toEqual('http://maps.apple.com/?ll=10.02134,-29.21322&z=15&q=pin%20name');
+	expect(createMapLink({ ...options, provider: 'apple' })).toEqual(`http://maps.apple.com/?ll=10.02134,-29.21322&z=15&q=${expectedQueryName}`);
+	expect(createMapLink({ ...options, provider: 'googleDeepLink' })).toEqual('comgooglemaps://?center=10.02134,-29.21322&zoom=15');
 });
 
 test('Create a delayed function', () => {
 	const link = createOpenLink(options);
+	console.log(link);
 	expect(typeof link).toBe('function');
 });
--- index.js
+++ index.js
@@ -7,24 +7,36 @@ export default function open({latitude, longitude, zoomLevel, name, provider}) {
 }
 
 export function createOpenLink({latitude, longitude, zoomLevel = 15, name, provider}) {
-	if (!provider) {
-		defaultProvider = (Platform.OS === 'ios') ? 'apple' : 'google';
+	// Returns a delayed async function that opens when executed
+	return async () => {
+		if (!provider) {
+			defaultProvider = (Platform.OS === 'ios') ? 'apple' : 'google';
+		}
+	
+		let mapProvider = provider || defaultProvider;
+		if (provider === 'google' && Platform.OS === 'ios') {
+			const supported = await Linking.canOpenURL('comgooglemaps://?center=40.765819,-73.975866&zoom=14&views=traffic');
+			mapProvider = 'googleDeepLink';
+		}
+		// Allow override provider, otherwise use the default provider
+		const mapLink = createMapLink({latitude, longitude, zoomLevel, name, provider: mapProvider});
+	
+		Linking.openURL(mapLink).catch(err => console.error('An error occurred', err));
 	}
-
-	const mapProvider = provider || defaultProvider;
-	// Allow override provider, otherwise use the default provider
-	const mapLink = createMapLink({latitude, longitude, zoomLevel, name, provider:  mapProvider});
-
-	// Returns a delayed function that opens when executed
-	return () => Linking.openURL(mapLink).catch(err => console.error('An error occurred', err));
 }
 
-export function createMapLink({latitude, longitude, zoomLevel = 15, name = 'Pin', provider = 'google'}) {
-	let pinname = encodeURI(name);
+export function createMapLink({latitude, longitude, zoomLevel = 15, query, provider = 'google'}) {
 	const link = {
-		'google': `http://maps.google.com/maps?ll=${latitude},${longitude}&z=${zoomLevel}&q=${pinname}`,
-		'apple': `http://maps.apple.com/?ll=${latitude},${longitude}&z=${zoomLevel}&q=${pinname}`
+		'google': `http://maps.google.com/maps?ll=${latitude},${longitude}&z=${zoomLevel}`,
+		'apple': `http://maps.apple.com/?ll=${latitude},${longitude}&z=${zoomLevel}`,
+		'googleDeepLink': `comgooglemaps://?center=${latitude},${longitude}&zoom=${zoomLevel}`,
 	};
+
+	if (query) {
+		const queryParam = `q=${encodeURI(query)}`;
+		link.google = link.google.concat('&', queryParam);
+		link.apple = link.apple.concat('&', queryParam);
+	}
 
 	return link[provider];
 }
