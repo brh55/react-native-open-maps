6b822a47f1ea27cb05a2a57dad31e81a5898fc23
brh55 <brandonhim@live.com>
2018-08-15 17:56:10 -0700

    Add start and end

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,4 +1,10 @@
-import { createMapLink, createOpenLink } from '../.';
+import { 
+	createMapLink,
+	createOpenLink,
+	geoCordStringify,
+	createQueryParameters,
+	createAppleParams
+} from '../.';
 
 const query = 'Yosemite National Park';
 const expectedQueryName = encodeURI(query);
@@ -9,6 +15,104 @@ const options = {
 	zoom: 15,
 }
 
+test('Stringify geocoordinates', () => {
+	expect(geoCordStringify(11.111,222.222)).toEqual('11.111,222.222');
+	expect(
+		geoCordStringify(
+			'hello fellow developer',
+			'if you are reading this, you are a great always remember that!')
+		.toThrow('Entered a non-number value for geo coordinates.')
+	);
+});
+
+const formattedParams = {
+	coords: '11.11,22.222',
+	zoomLevel: 10,
+	travelType: 'drive'
+};
+
+// Check to make sure it omits empty params
+const formattedParams2 = {
+	query: 'New%20York%20City',
+	coords: '',
+	zoomLevel: 5,
+	travelType: 'walk',
+	end: '',
+	start: ''
+};
+
+test('Create apple params', () => {
+	expect(
+		createAppleParams(formattedParams)
+	).toMatchObject({
+		z: 10,
+		dirflag: 'd',
+		ll: '11.11,22.222'
+	});
+	
+	expect(
+		createAppleParams(formattedParams2)
+	)
+	.toMatchObject({
+		q: 'New%20York%20City',
+		z: 5,
+		dirflag: 'w',
+	});
+});
+
+test('Create proper cross-platform query parameters', () => {
+	const base = {
+		latitude: 22.22,
+		longitude: 11.11,
+		zoomLevel: 10,
+		travelType: 'drive'
+	};
+
+	const baseExpected = {
+		apple: {
+			ll: '22.22,11.11',
+			z: parameters.zoomLevel,
+			dirflag: 'd'
+		},
+		google: {
+			query: '22.22,11.11',
+			zoom: parameters.zoomLevel,
+		}
+	};
+
+	const actualOutput = createMapParameters(parameters);
+	expect(actualOutput.google).toMatchObject(baseExpected.google);
+	expect(actualOutput.apple).toMatchObject(baseExpected.apple);
+
+	const directions = {
+		start: 'City Hall, New York City, NY',
+		end: 'SOHO, New York City, NY',
+		travelType: 'drive',
+	}
+
+	const directionInput = Object.assign({}, base, directions);
+	const escapeStart = encodeURI(directions.start);
+	const escapeEnd = encodeURI(directions.end);
+
+	const expectedDirection = {
+		apple: {
+			...baseExpected.apple,
+			saddr: escapeStart,
+			daddr: escapeEnd,
+			dirflag: 'd',
+		},
+		google: {
+			...baseExpected.google,
+			origin: escapeStart,
+			destination: escapeEnd,
+			travelmode: 'drive'
+		}
+	};
+
+	const directionOuput = createMapParameters(directionInput);
+	expect(directionOuput).toMatchObject(expectedDirection);
+});
+
 test('Create proper links', () => {
 	// Google default link
 	expect(createMapLink(options)).toEqual(`https://www.google.com/maps/search/?api=1&zoom=15&query=10.02134,-29.21322`);
@@ -16,6 +120,13 @@ test('Create proper links', () => {
 	expect(createMapLink({ ...options, provider: 'google', query })).toEqual(`https://www.google.com/maps/search/?api=1&zoom=15&query=${expectedQueryName}`);
 	// Apple map link
 	expect(createMapLink({ ...options, provider: 'apple', query })).toEqual(`http://maps.apple.com/?ll=10.02134,-29.21322&z=15&q=${expectedQueryName}`);
+});
+
+test('Create a cross-compatible origin direction link', () => {
+	const appleLink = createMapLink({
+		...options,
+
+	})
 });
 
 test('Create a delayed function', () => {
